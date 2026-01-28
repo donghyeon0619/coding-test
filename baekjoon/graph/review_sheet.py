07080a891d86a47386f23cc60bcb1a212e916831
DongHyeon Yoon <128972134+donghyeon0619@users.noreply.github.com>
2026-01-28 11:36:20 +0900

    [BOJ][2178] 문제 (미로) 복습

--- baekjoon/graph/review_sheet.py
+++ baekjoon/graph/review_sheet.py
@@ -3,27 +3,32 @@ from collections import deque
 
 def bfs(sx, sy):
 
-    global matrix, visited
+    global matrix, visited, dist, M, N
 
-    dx = [1, 0, -1, 0]
-    dy = [0, -1, 0, 1]
+    d = [(1, 0), (0, -1), (-1, 0), (0, 1)]
 
     q = deque()
 
-    cnt = 0
     q.append((sx, sy))
     visited[sy][sx] = True
+    dist[sy][sx] = 1
 
     while q:
         x, y = q.popleft()
-        for ndx, ndy in dx, dy:
-            if
+        for ndx, ndy in d:
+            nx, ny = x + ndx, y + ndy
+            if 0 < nx <= M and 0 < ny <= N and not visited[ny][nx] and matrix[ny][nx] == "1":
+                visited[ny][nx] = True
+                q.append((nx, ny))
+                dist[ny][nx] = dist[y][x] + 1
 
-
+    print(dist[N][M])
 
 
 N, M = map(int, input().split())
 
 matrix = ['0' * (M + 1)] + ['0' + input() for _ in range(N)]
 visited = [[False] * (M + 1) for _ in range(N + 1)]
+dist = [[0] * (M + 1) for _ in range(N + 1)]
 
+bfs(1, 1)
